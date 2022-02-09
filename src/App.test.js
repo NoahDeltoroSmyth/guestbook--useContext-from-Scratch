79a7b0621ab856e6b144a2f03e7da6f8bb8ea29f
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-08 17:35:43 -0800

    wrote 2 tests to check for user name in header and a user entry on the page after form interaction

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 import { EntryProvider } from './context/EntryContext';
 import { UserProvider } from './context/UserContext';
@@ -23,4 +24,54 @@ test('renders all components on page before a user inputs entries', () => {
 
   const signButton = screen.getByRole('button', { name: /sign here/i });
   expect(signButton).toBeInTheDocument();
+});
+
+test('renders users name in the header after user interacts with form', () => {
+  render(
+    <UserProvider>
+      <EntryProvider>
+        <App />
+      </EntryProvider>
+    </UserProvider>
+  );
+
+  const nameInput = screen.getByPlaceholderText(/name/i);
+  const entryInput = screen.getByPlaceholderText(/write a note/i);
+  const signButton = screen.getByRole('button', { name: /sign here/i });
+
+  const name = 'noah';
+  const entry = 'ello';
+
+  userEvent.type(nameInput, name);
+  userEvent.type(entryInput, entry);
+
+  userEvent.click(signButton);
+
+  const userHeader = screen.getByText(/signing the guestbook as noah/i);
+  expect(userHeader).toBeInTheDocument();
+});
+
+test('renders users entry on the page after user interacts with form', () => {
+  render(
+    <UserProvider>
+      <EntryProvider>
+        <App />
+      </EntryProvider>
+    </UserProvider>
+  );
+
+  const nameInput = screen.getByPlaceholderText(/name/i);
+  const entryInput = screen.getByPlaceholderText(/write a note/i);
+  const signButton = screen.getByRole('button', { name: /sign here/i });
+
+  const name = 'Anakin';
+  const entry = 'I am the chosen one';
+
+  userEvent.type(nameInput, name);
+  userEvent.type(entryInput, entry);
+
+  userEvent.click(signButton);
+
+  const userEntry = screen.getByText(/Anakin I am the chosen one/i);
+  expect(userEntry).toBeInTheDocument();
 });

8b6696738116d08be46ec21a3799161afe280ef4
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-09 12:58:11 -0800

    dark mode capable on app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useUser } from '../../context/UserContext';
+import DarkModeToggle from '../../hooks/DarkMode/DarkModeToggle';
 
 const Header = () => {
   const { user } = useUser();
@@ -7,6 +8,7 @@ const Header = () => {
     <>
       {user && <p>Signing the guestbook as {user}</p>}
       {!user && <p>Enter your name to write an entry!</p>}
+      <DarkModeToggle />
     </>
   );
 };

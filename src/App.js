e0cde6280d47de49386fe04f4f3fda5b3edcf887
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-21 17:24:01 -0800

    readme updated with rough plan, routes setup and router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import EntryList from './components/EntryList/EntryList';
 import Form from './components/Form/Form';
 import Header from './components/Header/Header';
+import Auth from './views/Auth';
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Form />
-      <EntryList />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Form />
+          <EntryList />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
